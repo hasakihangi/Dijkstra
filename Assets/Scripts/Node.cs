b4099b1c9d6798512e69197c2458426f40bd551c
hasakihangi <867174348@qq.com>
2026-03-12 01:13:52 +0800

    Update Node.cs

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 // 需要一个接口,
 
-public struct Node
+public struct Node: IComparable<Node>
 {
     public Vector2Int grid; // 这个是id..
     public long cost;
@@ -12,5 +12,10 @@ public struct Node
     {
         this.grid = grid;
         this.cost = cost;
+    }
+
+    public int CompareTo(Node other)
+    {
+        return cost.CompareTo(other.cost);
     }
 }
